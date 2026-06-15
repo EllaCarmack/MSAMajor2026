e890206749d8955a49cd336d7129a822fade851d
EllaCarmack <ellacarmack7@gmail.com>
2026-06-15 09:21:25 -0500

    Add files via upload

--- challenge_code/math_interpreter.py
+++ challenge_code/math_interpreter.py
@@ -3,42 +3,56 @@ def main():
     while True:
         #INPUT
         # prompt the user to enter an expression in the format X y Z (x and z are intergers and y is the sign)
-        user_input = print(input("Enter an expression: "))
+        user_input = input("Enter an expression: ")
 
         #PROCESS
         # valiate the expression format
         # use the split method to split the expression at the space " "
         expression_data = user_input.split(" ")
         # if the length of the resulting list is not 3 then invalid format
-        if len(user_input) != 3:
-            print("Invalid Format")
+        if len(expression_data) != 3:
+            print("ERROR: Invalid Format")
             continue
         
         # validate that X and Z are integers
-        X = user_input[0]
-        Z = user_input[2]
+        X = expression_data[0]
+        Y = expression_data[1]
+        Z = expression_data[2]
         # convert to int
         try:
-            int(user_input)
+            X = int(X)
+            Z = int(Z)
         # if converting causes an exception, then invalid format
         except:
-            print("Invalid Format")
-        
+            print("ERROR: Invalid Format")
 
-        
-   
+        # validate that Y is an acceptable operator (+, -, *, /), if statement to determine if Y == + or - or * or /
+        if Y != "+" and Y != "-" and Y != "*" and Y != "/":
 
-    
+            # invalid format if not
+            print("ERROR: Invalid Format")
+            continue
 
-    # validate that Y is an acceptable operator (+, -, *, /)
-        # use and if statement to determine if Y == + or - or * or /
-        # invalid format if not
+        # validate that when Y is "/" Z is not 0, use if: if Y == / and Z == 0: divide by 0 error
+        if Y == "/" and Z == 0:
+            print("ERROR: Divide by 0 error")
+            continue
+  
+        # do the math
+        if Y == "+":
+            answer = X + Z
+        if Y == "-":
+            answer = X - Z
+        if Y == "*":
+            answer = X * Z
+        if Y == "/":
+            answer = X / Z
+        #OUTPUT, print the output to the user
+        print(answer)
 
-    # validate that when Y is "/" Z is not 0
-        # use if: if Y == / and Z == 0: divide by 0 error
-
-    # do the math
-
-    #OUTPUT
-    #print the output to the user
+        re_prompt = input("Do you want to evaluate another expression? (Press y to continue):")
+        if re_prompt == "y":
+            continue
+        else:
+            break     
 main()
